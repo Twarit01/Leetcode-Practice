017bd6e680b196067da84b29d235d94caf6d863b
Twarit <twaritshukla.01@gmail.com>
2026-09-03 14:03:24 +0530

    Accepted: Build an Array With Stack Operations (attempt 1)

---
+++ Solved_Questions/1441-build-an-array-with-stack-operations/attempt-1-accepted.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> res = new ArrayList<>();
+        int curr = 1;
+        for (int t : target) {
+            while (curr < t) {
+                res.add("Push");
+                res.add("Pop");
+                curr++;
+            }
+            res.add("Push");
+            curr++;
+        }
+        return res;
+    }
+}
