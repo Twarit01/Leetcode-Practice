328c7a99fb4c8183d4118b42a9afb45a1cb51043
Twarit <twaritshukla.01@gmail.com>
2026-09-03 14:03:57 +0530

    Accepted: Implement Stack using Queues (attempt 3)

---
+++ Solved_Questions/225-implement-stack-using-queues/attempt-3-accepted.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class MyStack {
+    Queue<Integer> q = new LinkedList<>();
+    public void push(int x) {
+        q.add(x);
+        for (int i = 0; i < q.size() - 1; i++)
+            q.add(q.poll());
+    }
+    public int pop() {
+        return q.poll();
+    }
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
