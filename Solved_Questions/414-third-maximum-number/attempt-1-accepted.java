47adc92e64fb134833755fe96983d6d98f7edd72
Twarit <twaritshukla.01@gmail.com>
2026-09-03 13:53:29 +0530

    Accepted: Third Maximum Number (attempt 1)

---
+++ Solved_Questions/414-third-maximum-number/attempt-1-accepted.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        Long max1 = null, max2 = null, max3 = null;
+
+        for (int n : nums) {
+            long num = n; // convert int → long
+
+            if ((max1 != null && num == max1) ||
+                (max2 != null && num == max2) ||
+                (max3 != null && num == max3)) {
+                continue;
+            }
+
+            if (max1 == null || num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (max2 == null || num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (max3 == null || num > max3) {
+                max3 = num;
+            }
+        }
+
+        return max3 == null ? max1.intValue() : max3.intValue();
+    }
+}
