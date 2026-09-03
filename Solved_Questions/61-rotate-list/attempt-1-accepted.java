b9226960c4b9e6a08c57b7a4ec226199e6976b35
Twarit <twaritshukla.01@gmail.com>
2026-09-03 14:13:30 +0530

    Accepted: Rotate List (attempt 1)

---
+++ Solved_Questions/61-rotate-list/attempt-1-accepted.java
@@ -0,0 +1,37 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        int length = 1;
+        ListNode tail = head;
+
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        k = k % length;
+
+        if (k == 0) {
+            return head;
+        }
+
+        tail.next = head;
+
+        int steps = length - k;
+
+        ListNode newTail = tail;
+
+        while (steps > 0) {
+            newTail = newTail.next;
+            steps--;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
